5fdb7bbf823a861929e4f0b532612df1b8a7e763
Joosung Park <iam@hyperlapse.dev>
2024-04-19 00:20:00 +0900

    chore: add v0 component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+
+import { DM_Sans } from 'next/font/google'
+import { Inter } from "next/font/google";
+
+const dmSans=DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${dmSans.className}`}>{children}</body>
     </html>
   );
 }
